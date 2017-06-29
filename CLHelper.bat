631eea07eeafc38bb12c3918c151e0e7b91fb7af
GaryLBaird <gary_baird@msn.com>
2017-06-29 10:28:07 -0600

    Update CLHelper.bat

--- CLHelper.bat
+++ CLHelper.bat
@@ -92,6 +92,99 @@ ENDLOCAL && SET AliasFile=%_AliasFile_%\alias.cmd
 CALL:FORMATOUT 12,12,"%~1","Created File:%AliasFile%.alias.cmd"
 GOTO:EOF
 
+:--BestColor
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET "BackBroundColor=%~1"
+SET "TextColor=%~2"
+REM SET DEFAULTS:
+IF NOT DEFINED BackBroundColor SET BackBroundColor=5
+IF NOT DEFINED TextColor SET TextColor=E
+
+IF "%~1"=="" GOTO :BestColorDone
+IF /I "Black"=="%~1" SET BackBroundColor=0
+IF /I "Black"=="%~2" SET TextColor=0
+IF /I "Gray"=="%~1" SET BackBroundColor=8
+IF /I "Gray"=="%~2" SET TextColor=8
+IF /I "Grey"=="%~1" SET BackBroundColor=8
+IF /I "Grey"=="%~2" SET TextColor=8
+IF /I "Blue"=="%~1" SET BackBroundColor=1
+IF /I "Blue"=="%~2" SET TextColor=1
+IF /I "Light Blue"=="%~1" SET BackBroundColor=9
+IF /I "Light Blue"=="%~2" SET TextColor=9
+IF /I "lBlue"=="%~1" SET BackBroundColor=9
+IF /I "LBlue"=="%~2" SET TextColor=9
+IF /I "Green"=="%~1" SET BackBroundColor=2
+IF /I "Green"=="%~2" SET TextColor=2
+IF /I "Light Green"=="%~1" SET BackBroundColor=A
+IF /I "Light Green"=="%~2" SET TextColor=A
+IF /I "LGreen"=="%~1" SET BackBroundColor=A
+IF /I "LGreen"=="%~2" SET TextColor=A
+IF /I "Aqua"=="%~1" SET BackBroundColor=3
+IF /I "Aqua"=="%~2" SET TextColor=3
+IF /I "Light Aqua"=="%~1" SET BackBroundColor=B
+IF /I "Light Aqua"=="%~2" SET TextColor=B
+IF /I "LAqua"=="%~1" SET BackBroundColor=B
+IF /I "LAqua"=="%~2" SET TextColor=B
+IF /I "Red"=="%~1" SET BackBroundColor=4
+IF /I "Red"=="%~2" SET TextColor=4
+IF /I "Light Red"=="%~1" SET BackBroundColor=C
+IF /I "Light Red"=="%~2" SET TextColor=C
+IF /I "LRed"=="%~1" SET BackBroundColor=C
+IF /I "LRed"=="%~2" SET TextColor=C
+IF /I "Purple"=="%~1" SET BackBroundColor=5
+IF /I "Purple"=="%~2" SET TextColor=5
+IF /I "Light Purple"=="%~1" SET BackBroundColor=D
+IF /I "Light Purple"=="%~2" SET TextColor=D
+IF /I "LPurple"=="%~1" SET BackBroundColor=D
+IF /I "LPurple"=="%~2" SET TextColor=D
+IF /I "Yellow"=="%~1" SET BackBroundColor=6
+IF /I "Yellow"=="%~2" SET TextColor=6
+IF /I "Light Yellow"=="%~1" SET BackBroundColor=E
+IF /I "Light Yellow"=="%~2" SET TextColor=E
+IF /I "LYellow"=="%~1" SET BackBroundColor=E
+IF /I "LYellow"=="%~2" SET TextColor=E
+IF /I "White"=="%~1" SET BackBroundColor=7
+IF /I "White"=="%~2" SET TextColor=7
+IF /I "Bright White"=="%~1" SET BackBroundColor=F
+IF /I "Bright White"=="%~2" SET TextColor=F
+IF /I "LWhite"=="%~1" SET BackBroundColor=F
+IF /I "LWhite"=="%~2" SET TextColor=F
+:: Yellow Text Purple Background 
+:BestColorDone
+Color !BackBroundColor!!TextColor!
+ENDLOCAL
+CLS
+GOTO:EOF
+
+:--RandomColor
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET /a LETTER=(%Random% %%6)+1
+SET /a NUMBER=(%Random% %%9)+1
+GOTO :CASE_!LETTER!
+:CASE_1
+  SET RETURN=A
+  GOTO :END_CASE
+:CASE_2
+  SET RETURN=B
+  GOTO :END_CASE
+:CASE_3
+  SET RETURN=C
+  GOTO :END_CASE
+:CASE_4
+  SET RETURN=D
+  GOTO :END_CASE
+:CASE_5
+  SET RETURN=E
+  GOTO :END_CASE
+:CASE_6
+  SET RETURN=F
+  GOTO :END_CASE
+:END_CASE
+
+ENDLOCAL && set RETURN=%return% && SET NUMBER=%NUMBER%
+IF DEFINED RETURN COLOR %NUMBER%%RETURN%
+GOTO:EOF
+
 :: Help Content Below
 :--Help
 CALL:FORMATOUT 20,20,"---------------------------","------------------------------------------------------"
@@ -99,11 +192,15 @@ CALL:FORMATOUT 20,20,"File:%SELF_0%","Options and Usage Help."
 CALL:FORMATOUT 20,20,"---------------------------","------------------------------------------------------"
 CALL:FORMATOUT 20,20,"Options:","Description%~0"
 CALL:FORMATOUT 20,20,"--About","Describes the author and purpose."
+CALL:FORMATOUT 20,20,"--BestColor","Sets the color of the command window."
+CALL:FORMATOUT 20,20," ..  Usage:","%SELF_0% --BestColor Background_Color Text_Color"
 CALL:FORMATOUT 20,20,"--CreateAliasFile","Creates the alias file."
 CALL:FORMATOUT 20,20," ..","Every time a command windows loads this alias.cmd file"
 CALL:FORMATOUT 20,20," .."," will setup and configure the working environment."
 CALL:FORMATOUT 20,20," .."," This is done through a registry key which will be"
 CALL:FORMATOUT 20,20," .."," created or modified."
+CALL:FORMATOUT 20,20,"--RandomColor","Randomly picks and sets the color of the command window."
+CALL:FORMATOUT 20,20," ..  Usage:","%SELF_0% --RandomColor Background_Color Text_Color"
 CALL:FORMATOUT 20,20,"--Help","Displays this help menu."
 CALL:FORMATOUT 20,20,"---------------------------","------------------------------------------------------"
 GOTO:EOF
