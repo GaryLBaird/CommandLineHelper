a0ea0615d210a31abeec5775ca8bfd1fd0c07c9e
Gary Baird <gary.baird@academymortgage.com>
2017-07-13 14:07:42 -0600

    Making minor changes to the scripts to fix an issue with functions calling other functions incorrectly.

--- setup.bat
+++ setup.bat
@@ -35,29 +35,27 @@ REM :: Everything below this line will only be called if a function above has be
 REM :: Formatout is an internal utility to format text.
 REM :: Small Text Formatter Code Begin
 
-:FORMATOUT
-SETLOCAL ENABLEDELAYEDEXPANSION
-  SET __Left__=%~1
-  SET __RIGHT__=%~2
-  SET "__TEXT__=%~3"
-  SET "__OTHER__=%~4 %~5 %~6"
-  SET "spaces=                                                                                                                    "
-  SET /A __SIZE__=10
-  CALL:PADRIGHT __TEXT__ %__Left__%
-  CALL:PADLEFT __SIZE__ %__RIGHT__%
-  REM ECHO %__TEXT__%+%__SIZE__%+%__OTHER__%
-  ECHO. %__TEXT__% %__OTHER__%
-ENDLOCAL
+:PADRIGHT
+  CALL SET padded=%%%1%%%spaces%
+  CALL SET %1=%%padded:~0,%2%%
 GOTO:EOF
 
-:PADRIGHT
-CALL SET padded=%%%1%%%spaces%
-CALL SET %1=%%padded:~0,%2%%
-GOTO:eof
-
 :PADLEFT
-CALL SET padded=%spaces%%%%1%%
-CALL SET %1=%%padded:~-%2%%
+  CALL SET padded=%spaces%%%%1%%
+  CALL SET %1=%%padded:~-%2%%
+GOTO:EOF
+
+:FORMATOUT
+SET __Left__=%~1
+SET __RIGHT__=%~2
+SET "__TEXT__=%~3"
+SET "__OTHER__=%~4 %~5 %~6"
+SET "spaces=                                                                                                                    "
+SET /A __SIZE__=10
+CALL:PADRIGHT __TEXT__ %__Left__%
+CALL:PADLEFT __SIZE__ %__RIGHT__%
+REM ECHO %__TEXT__%+%__SIZE__%+%__OTHER__%
+ECHO. %__TEXT__% %__OTHER__%
 GOTO:EOF
 
 REM :: Small Text Formatter Code End
@@ -156,6 +154,7 @@ SETLOCAL ENABLEDELAYEDEXPANSION
     CALL:FORMATOUT 50,50,"Results:!_DESTINATION_!","Was not copied."
   )
 ENDLOCAL
+:: DONE!
 GOTO:EOF
 
 
@@ -249,57 +248,55 @@ GOTO:EOF
 REM :: Help Content Below
 
 :--Help
-REM SETLOCAL ENABLEDELAYEDEXPANSION
-  IF /I "%ARGS%" GEQ "--Help" (
-    CLS
-    CALL:FORMATOUT 20,20," ---------------------------","------------------------------------------------------"
-    CALL:FORMATOUT 20,20," File: %SELF_0%"," Options and Usage Help."
-    CALL:FORMATOUT 20,20,"---------------------------","------------------------------------------------------"
-    CALL:FORMATOUT 20,20," Options:","Description '%~0'"
-    CALL:FORMATOUT 20,20," --About","Describes the author and purpose."
-    CALL:FORMATOUT 20,20," --Alias-Remove","Removes the alias key to the registry."
-    CALL:FORMATOUT 20,20," --AliasFile","Adds the alias file to the registry."
-    CALL:FORMATOUT 20,20," ..","Every time a command windows loads this alias.cmd file"
-    CALL:FORMATOUT 20,20," .."," will setup and configure the working environment."
-    CALL:FORMATOUT 20,20," .."," This is done through a registry key which will be"
-    CALL:FORMATOUT 20,20," .."," created or modified."
-    CALL:FORMATOUT 20,20," --Copy","Copies a file and creates destination directory if missing."
-    CALL:FORMATOUT 20,20," ..","Users will be prompted if the file needs to be overwritten."
-    CALL:FORMATOUT 20,20," ..  Usage:","%SELF_0% c:\directory\filename.name c:\destination"
-    CALL:FORMATOUT 20,20," --Help","Displays this help menu."
-    CALL:FORMATOUT 20,20," --Install","Installs CommandLineHelper."
-    CALL:FORMATOUT 20,20," ..  NOTE:"," You must run "SET ADD_REG=True" from the comandline to install the"
-    CALL:FORMATOUT 20,20," ..       "," registry key. The registry key must be set in order to have the"
-    CALL:FORMATOUT 20,20," ..       "," alias.cmd load everytime a command window has been launched ."
-    CALL:FORMATOUT 20,20,"---------------------------","------------------------------------------------------"
-  )
-REM ENDLOCAL
-GOTO :HelpDone
-:HelpDone
+IF /I "%ARGS%" GEQ "--Help" (
+  CLS
+  CALL:FORMATOUT 20,20," ---------------------------","------------------------------------------------------"
+  CALL:FORMATOUT 20,20," File: %SELF_0%"," Options and Usage Help."
+  CALL:FORMATOUT 20,20,"---------------------------","------------------------------------------------------"
+  CALL:FORMATOUT 20,20," Options:","Description '%~0'"
+  CALL:FORMATOUT 20,20," --About","Describes the author and purpose."
+  CALL:FORMATOUT 20,20," --Alias-Remove","Removes the alias key to the registry."
+  CALL:FORMATOUT 20,20," --AliasFile","Adds the alias file to the registry."
+  CALL:FORMATOUT 20,20," ..","Every time a command windows loads this alias.cmd file"
+  CALL:FORMATOUT 20,20," .."," will setup and configure the working environment."
+  CALL:FORMATOUT 20,20," .."," This is done through a registry key which will be"
+  CALL:FORMATOUT 20,20," .."," created or modified."
+  CALL:FORMATOUT 20,20," --Copy","Copies a file and creates destination directory if missing."
+  CALL:FORMATOUT 20,20," ..","Users will be prompted if the file needs to be overwritten."
+  CALL:FORMATOUT 20,20," ..  Usage:","%SELF_0% c:\directory\filename.name c:\destination"
+  CALL:FORMATOUT 20,20," --Help","Displays this help menu."
+  CALL:FORMATOUT 20,20," --Install","Installs CommandLineHelper."
+  CALL:FORMATOUT 20,20," ..  NOTE:"," You must run "SET ADD_REG=True" from the comandline to install the"
+  CALL:FORMATOUT 20,20," ..       "," registry key. The registry key must be set in order to have the"
+  CALL:FORMATOUT 20,20," ..       "," alias.cmd load everytime a command window has been launched ."
+  CALL:FORMATOUT 20,20,"---------------------------","------------------------------------------------------"
+)
 GOTO:EOF
 
 REM :: Author Information Below
 
 :--About
-SETLOCAL ENABLEDELAYEDEXPANSION
-  IF /I "%ARGS%" GEQ "--About" (
-    CLS
-    CALL:FORMATOUT 20,20," ---------------------------","------------------------------------------------------"
-    CALL:FORMATOUT 20,20," Author:","Gary L Baird"
-    CALL:FORMATOUT 20,20," Written by:","Gary L Baird"
-    CALL:FORMATOUT 20,20," Phone:","TBA"
-    CALL:FORMATOUT 20,20," Email:","TBA"
-    CALL:FORMATOUT 20,20," Filename:","%SELF_0%"
-    CALL:FORMATOUT 20,20," Purpose:","Make the Windows Command Line more friendly."
-    CALL:FORMATOUT 20,20," Project:","Part of the Command Line Helper project."
-    CALL:FORMATOUT 20,20," Location:","github.com/GaryLBaird/CommandLineHelper"
-    CALL:FORMATOUT 20,20," License:","GNU GENERAL PUBLIC LICENSE"
-    CALL:FORMATOUT 20,20," ---------------------------","------------------------------------------------------"
-  )
-ENDLOCAL
-GOTO :AboutDone
-:AboutDone
+IF /I "%ARGS%" GEQ "--About" (
+  CLS
+  CALL:FORMATOUT 20,20," ---------------------------","------------------------------------------------------"
+  CALL:FORMATOUT 20,20," Author:","Gary L Baird"
+  CALL:FORMATOUT 20,20," Written by:","Gary L Baird"
+  CALL:FORMATOUT 20,20," Phone:","TBA"
+  CALL:FORMATOUT 20,20," Email:","TBA"
+  CALL:FORMATOUT 20,20," Filename:","%SELF_0%"
+  CALL:FORMATOUT 20,20," Purpose:","Make the Windows Command Line more friendly."
+  CALL:FORMATOUT 20,20," Project:","Part of the Command Line Helper project."
+  CALL:FORMATOUT 20,20," Location:","github.com/GaryLBaird/CommandLineHelper"
+  CALL:FORMATOUT 20,20," License:","GNU GENERAL PUBLIC LICENSE"
+  CALL:FORMATOUT 20,20," ---------------------------","------------------------------------------------------"
+)
 GOTO:EOF
+
+REM :FORMATOUT
+REM CALL:--FORMATOUT "%~1","%~2","%~3","%~4","%~5%~6%~7"
+REM goto :formatdone
+REM :formatdone
+REM GOTO:EOF
 
 :DONE
 
